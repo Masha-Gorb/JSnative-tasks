88a9f389d5866e39cdfca5959d3c3748fb949543
Masha Gorbunova <masha.gorbunova@gmail.com>
2021-11-28 13:49:46 +0300

    add deepCopy

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+// N A T I V E JS LESSON1
 //Объекты: объекты (проверяем typeof), массивы (проверяем .isArray), функции (проверяем typeof)
+//let a = [];
+//console.log((Array.isArray(a)));
+// let a = {} — литерал объекта, который тыры пыры приводит к созданию объекта в памяти браузера
 
-let a = [];
-console.log((Array.isArray(a)));
+let student = {
+    name: 'Ann',
+    friends: ['Olga', 'Loren', 'Vera']
+}
+
+let copyStudent = {...student};
+console.log(student);
+console.log(copyStudent);
+copyStudent.friends.push('Mary');
+console.log(student);
+console.log(copyStudent);
+let deepCopyStudent = {...student, friends: [...student.friends, 'Carla']}
+console.log(deepCopyStudent);
+console.log(student)
