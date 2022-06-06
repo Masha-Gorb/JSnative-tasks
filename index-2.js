ea41f18aa247ae901ca3f1015afc85feaf3a8fb1
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-06-06 22:30:46 +0300

    8kyu kata

--- index-2.js
+++ index-2.js
@@ -450,3 +450,14 @@ console.log(
 //         // console.log(acc+n)
 //         return acc+n
 // }, 100)
+
+//ката 8 куи. дана строка, надо вернуть строку с поменяными местами словами
+//решение мое
+function nameSuffle(str){
+    var arr = str.split(" ");
+    return arr[1] + ' ' + arr[0]
+}
+//бест практис
+function nameSuffle(str){
+  return str.split(' ').reverse().join(' ')
+}
