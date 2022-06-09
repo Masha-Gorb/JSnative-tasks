461f439f43294c0dfbcc8e3aa90610a773bd09cc
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-06-09 14:29:44 +0300

    7kyu kata

--- index-2.js
+++ index-2.js
@@ -479,3 +479,19 @@ console.log(
 //         return false
 //     }
 // }
+
+//7куи ката: из длинного числа надо вернуть длинное число где все числа в квадрате
+// //размышления :)))
+// let num = 3212
+// let strNum = num.toString()
+// let arr11 = strNum.split('').map(m => +m * +m).join(' ').replace(/\s/g, '')
+// let result = parseInt(arr11)
+// console.log(arr11.replace(/\s/g, ''))
+// console.log(result)
+// //решение :
+// function squareDigits(num){
+//     let result = num.toString().split('').map(m => +m * +m).join(' ').replace(/\s/g, '')
+//     return parseInt(result)
+// }
+
+console.log('new task')
