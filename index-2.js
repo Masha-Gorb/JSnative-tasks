1b51f0b0fd545426821bcec8af7b1bb20b260a5e
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-06-10 14:03:11 +0300

    7kyu kata

--- index-2.js
+++ index-2.js
@@ -495,3 +495,8 @@ console.log(
 // }
 
 console.log('new task')
+//7куи сложная таска: дано число и массив функций, надо прогнать число через все функции в массиве
+//решение
+//function chain(input, fs) {
+//   return fs.reduce(function(input, fs) { return fs(input) }, input)
+// }
