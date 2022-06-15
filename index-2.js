dc34b3a8a9880ad0b0e5323789ca6b4183adbf7b
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-06-15 14:01:42 +0300

    7kyu kata - sucessfull solution :)))

--- index-2.js
+++ index-2.js
@@ -577,5 +577,19 @@ console.log('new task')
 //    return theArray;
 //   }
 
-console.log('new task')
+//7куи ката: дан массив и число, надо вернуть массив из прошлого массива тока четные столько же сколько число
+// let arrForThisKata = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// //[2, 4, 6, 8] => [4, 6, 8] - должно так стать
+// let numForThisKata = 3
+//
+// function evenNumbers(array, number) {
+//     let newArr = array.filter(f => f%2 === 0).reverse()
+//     newArr.length = number
+//     return newArr.reverse()
+// }
+// console.log(evenNumbers(arrForThisKata, numForThisKata))
+//решение выше подходит
+//решение ниже как пример более элегантного решения
+//const evenNumbers = (array, number) => array.filter(item => item % 2 === 0).slice(-number);
 
+console.log('new task')
