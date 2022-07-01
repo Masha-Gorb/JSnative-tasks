6649fd366402f8c4bab74dc695a438783c5b091d
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-07-01 18:56:35 +0300

    8kyu kata - exploring arr.find()

--- index-2.js
+++ index-2.js
@@ -444,7 +444,7 @@ console.log(
             return acc
         }, {}) //{}
 )
- //acc - место где будем накапливать значение
+//acc - место где будем накапливать значение
 //     [10, 20, 30].reduce((acc, n, i, arr) => {
 //         // console.log(acc, n, i, arr)
 //         // console.log(acc+n)
@@ -691,4 +691,23 @@ console.log(
 // }
 
 console.log('new task')
+//8kyu task мудреная таска фу
+let sheepArr = ["sheep", "sheep", "sheep", "wolf", "sheep", 'wolf']
 
+function foo(arr) {
+    let arr2 = arr.reverse()
+    if (arr2[0] !== 'wolf') {
+        function IsWolf(element) {
+            const danger = 'wolf'
+            return element === danger
+        }
+
+        let a = arr2.findIndex(IsWolf)
+        return 'Oi! Sheep number ' + a + '! You are about to be eaten'
+    } else {
+        return 'Pls go away and stop eating my sheep'
+    }
+
+}
+
+console.log(foo(sheepArr))
