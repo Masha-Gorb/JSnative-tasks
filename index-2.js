9ed208d02d926a26c347a52314787094233bb4f1
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-07-04 13:17:47 +0300

    7kyu kata - to sum all el in array use reduce!

--- index-2.js
+++ index-2.js
@@ -722,5 +722,12 @@ console.log(
 //     }
 // }
 
+//7kyu kata: приходит [[10,0],[3,5],[5,8]] - надо сложить числа во всех вложенных массивах
+//а затем сложить все элементы массива -.reduce((a,b)=>a+b)
+
+// let busStops = [[10,0],[3,5],[5,8]]
+// let testRes = busStops.map(m => m[0]-m[1]).reduce((a,b)=>a+b)
+// console.log(testRes)
 console.log('new task')
+
 
