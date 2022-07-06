c9c566f477920846b83df79c83933b465b89af7f
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-07-06 14:10:22 +0300

    7kyu kata - task with Object - very usefull!!!

--- index-2.js
+++ index-2.js
@@ -729,32 +729,32 @@ console.log(
 // let testRes = busStops.map(m => m[0]-m[1]).reduce((a,b)=>a+b)
 // console.log(testRes)
 
-console.log('new task')
 //7kyu task
+//задачка с объектом!
+//ВАЖНО
+// Object.values(нужный объект) = массив из всех значений
+//Object.keys(нужный объект) = массив всех ключей
+//объект[нужное свойство строкой] - если надо достучаться до значения извне
 
-let a = {
-    'tim': 1,
-    'jim': 3,
-    'randy': 9,
-    'sandy': 6,
-    'andy': 7,
-    'katie': 6,
-    'laura': 9,
-    'saajid': 9,
-    'alex': 9,
-    'john': 9,
-    'mr': 8
-}
+// let a = {'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}
+// let bossName = 'laura'
 
-let d = 'katie'
-let b = a.d
-console.log(b)
-// let meetAsArray = Object.values(a).reduce((a,b) => a+b )
-//
-// console.log(meetAsArray/11)
+// let sumOfAllScores = Object.values(a).reduce((a,b) => a+b) + a[boss]
+// let result = sumOfAllScores/11
 
-// function outed(meet, boss) {
-//     let allMemberScore = meet.map(m => m.key)
+// function outed(meet, boss){
+//     let sumOfAllScores = Object.values(meet).reduce((a,b) => a+b) + meet[boss]
+//     let result = Math.round(sumOfAllScores) / 11
+//     console.log(result)
+//     if(result <= 5) {
+//         return 'Get Out Now!'
+//     } else
+//         return 'Nice Work Champ!'
 // }
+// console.log(outed(a, bossName))
+
+console.log('new task')
+
+
 
 
