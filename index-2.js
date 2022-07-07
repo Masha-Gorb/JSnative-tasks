21f8bf9377abe17850c06e7d03ea519f1818974e
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-07-07 20:30:06 +0300

    8 kyu kata

--- index-2.js
+++ index-2.js
@@ -753,7 +753,15 @@ console.log(
 // }
 // console.log(outed(a, bossName))
 
+//8kyu task найти ближайшее квадратное число к вводимому числу
+//new Math.pow !!
+
+// let foo1 = (num) => {
+//    return Math.pow(Math.round(Math.sqrt(num)), 2)
+//
+
 console.log('new task')
+
 
 
 
