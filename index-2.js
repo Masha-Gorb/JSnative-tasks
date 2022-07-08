9a9881fa537d019134644d9f65d39dadfd4cbea7
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-07-08 14:57:37 +0300

    8 kyu kata

--- index-2.js
+++ index-2.js
@@ -761,7 +761,16 @@ console.log(
 //
 
 console.log('new task')
-
+//8 kyu task
+// function foo (p1, p2) {
+//     if(p1 === 'rock' && p2 === 'scissors') {
+//         return 'p2 won'
+//     } else {
+//         return 'Draw!'
+//     }
+// }
+//
+// console.log(foo('rock', 'rock'))
 
 
 
