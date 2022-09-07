a3552e1527b5ebd9b62fbbb6a744be11b6d26d3e
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-09-07 21:15:27 +0300

    add -> 8kyu kata solution update

--- 8kyu katas solutions/Do I get a bonus?.js
+++ 8kyu katas solutions/Do I get a bonus?.js
@@ -1,6 +1,6 @@
 function bonusTime(salary, bonus) {
 // your code here
-    return bonus ? "\u00A3"+salary*10 : salary
+    return bonus ? "\u00A3"+salary*10 : `£${salary}`
 }
 
 console.log(bonusTime(10000, true))
