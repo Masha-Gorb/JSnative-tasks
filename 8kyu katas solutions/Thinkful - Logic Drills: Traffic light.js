7b4745e4689b4fde1bc9278f619fd06d515712b3
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-09-26 17:11:12 +0300

    add -> 8kyu kata solution

---
+++ 8kyu katas solutions/Thinkful - Logic Drills: Traffic light.js
@@ -0,0 +1,9 @@
+function updateLight(current) {
+    if(current === 'green') {
+        return 'yellow'
+    } else if (current === 'yellow') {
+        return 'red'
+    } else {
+        return 'green'
+    }
+}
